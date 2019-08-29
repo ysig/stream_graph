0ee30bcc776a5e3c08cbe7c45799c4dfcc46d528
ysig <y.siglidis@gmail.com>
2019-08-29 18:15:10 +0200

    _common_time_descrete sequential alg + comments

--- stream_graph/base/itemporal_node_set_df.py
+++ stream_graph/base/itemporal_node_set_df.py
@@ -45,11 +45,14 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                 self.df_ = df
                 self.sort_by = sort_by
                 if isinstance(df, self.__class__):
+                    # Extract discrete if it is already there
                     discrete = df.discrete
                 if discrete is None:
+                    # By default it is None
                     discrete = False
                 if discrete and self.df_['ts'].dtype.kind != 'i' and self.df_['tf'].dtype.kind != 'i':
                     warn('SemanticWarning: For a discrete instance time-instants should be an integers')
+        # If dscrete is not initialized, set it to the default value 1.
         self.discrete_ = True if discrete is None else discrete
 
     @property
@@ -78,6 +81,7 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
         return (hasattr(self, 'sort_by_') and hasattr(self, 'sorted_') and self.sorted_) or self.sort_by_ is None
 
     def sort_df(self, sort_by):
+        """Return a sorted version of the data-frame given an order or retrieves it if already sorted."""
         if self.sort_by is None:
             self.sort_by = sort_by
             return self.sort_df(sort_by)
@@ -106,7 +110,7 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
     def nodeset(self):
         if not bool(self):
             return NodeSetS()
-        return NodeSetS(self.df_.u.drop_duplicates().values.flat)
+        return NodeSetS(self.df_.u.drop_duplicates())
 
     @property
     def df(self):
@@ -119,7 +123,7 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
     def timeset(self):
         if not bool(self):
             return ITimeSetS()
-        return ITimeSetS(self.df_[['ts']].values.flat, discrete=self.discrete)
+        return ITimeSetS(self.df_.ts, discrete=self.discrete)
 
     def __iter__(self):
         if bool(self):
@@ -150,9 +154,12 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                     assert ns.discrete == self.discrete
                     if not isinstance(ns, self.__class__):
                         try:
+                            # If not an instance of this class,
+                            # see if the other class has a method.
                             return ns & self
                         except NotImplementedError:
                             pass
+                    # Or else convert from its iterator form.
                     df = utils.ins_to_idf(ns).intersection(self.df)
                     if not df.empty:
                         return self.__class__(df, discrete=self.discrete)
@@ -170,9 +177,12 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                 if isinstance(ns, ABC.ITemporalNodeSet):
                     if not isinstance(ns, self.__class__):
                         try:
+                            # If not an instance of this class,
+                            # see if the other class has a method.
                             return ns | self
                         except NotImplementedError:
                             pass
+                    # Or else convert from its iterator form.
                     return self.__class__(self.df.union(utils.ins_to_idf(ns)), discrete=self.discrete)
                 else:
                     return ns | self
@@ -188,8 +198,11 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                 if ns:
                     assert ns.discrete == self.discrete
                     if isinstance(ns, ABC.ITemporalNodeSet):
+                        # If we have an instantaneous object - take the difference, by extracting the df
                         return self.__class__(self.df.difference(utils.ins_to_idf(ns)), discrete=self.discrete)
                     else:
+                        # If we don't have, convert our object to a non-instantaneous object,
+                        # subtract and then by converting back to our to our original type, we will have our valid result.
                         df = self.df
                         df['tf'] = df['ts']
                         return self.__class__((TemporalNodeSetDF(df, discrete=self.discrete) - ns).df.drop(columns=['tf']), discrete=self.discrete)
@@ -202,9 +215,11 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
     def n_at(self, t=None):
         if bool(self):
             if t is None:
+                # Count how many times its time-step occurs, for each key and return a sorted list 
                 return TimeCollection(sorted(list(iteritems(Counter(t for t in self.df.ts)))), instantaneous=True, discrete=self.discrete)
             elif isinstance(t, Real):
-                return len(set(self.df.df_at(t).u.values.flat))
+                # Count for only one time-stamp
+                return len(set(self.df.df_at(t).u))
             else:
                 raise ValueError('Input can either be a real number or an ascending interval of two real numbers')
         else:
@@ -228,9 +243,11 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                             active_set.add(u)
                     if len(active_set):
                         yield (prev, NodeSetS(set(active_set)))
-                return TimeGenerator(generate(self.df_.sort_values(by='ts').itertuples(name=None, index=False)), instantaneous=True, discrete=self.discrete)
+                # Iterate in ascending time and yield the NodeSet at each time-instant in a generator fashion
+                return TimeGenerator(generate(self.sort_df('ts').itertuples()), instantaneous=True, discrete=self.discrete)
             elif isinstance(t, Real):
-                return NodeSetS(self.df.df_at(t).u.values.flat)
+                # Count how many times its time-step occurs, for each key and return a sorted list 
+                return NodeSetS(self.df.df_at(t).u)
             else:
                 raise ValueError('Input can either be a real number or an ascending interval of two real numbers')
         else:
@@ -245,6 +262,7 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
                 times = defaultdict(set)
                 for u, ts in iter(self):
                     times[u].add(ts)
+                # Make a time-set for each node
                 return NodeCollection({u: ITimeSetS(s, discrete=self.discrete) for u, s in iteritems(times)})
             else:
                 return ITimeSetS(self.df[self.df.u == u]['ts'].values.flat, discrete=self.discrete)
@@ -296,38 +314,65 @@ class ITemporalNodeSetDF(ABC.ITemporalNodeSet):
 
     def _common_time_discrete(self, u=None):
         if u is None or self._common_time__list_input(u):
+            prev = None
             ct = defaultdict(int)
-            carrier = defaultdict(set)
-            for v, ts in iter(self):
-                carrier[ts].add(v)
             if u is None:
-                for ts, us in iteritems(carrier):
-                    if len(us) > 1:
-                        for v in us:
-                            ct[v] += (len(us)-1)
+                # If we want the common-time for all nodes
+                for u, ts in self.sorted_df('ts').itertuples():
+                    if prev is None:
+                        active_set, prev = {u}, ts
+                    elif ts != prev:
+                        if len(active_set) > 1:
+                            for v in active_set:
+                                # update their common time to the ammount of all the other coexisting nodes
+                                ct[v] += (len(active_set)-1)
+                        active_set, prev = {u}, ts
+                    else:
+                        active_set.add(u)
+                    if len(active_set) > 1:
+                        for v in active_set:
+                            # update their common time to the ammount of all the other coexisting nodes
+                            ct[v] += (len(active_set)-1)
             else:
-                valid_nodes = set(u)
-                for ts, us in iteritems(carrier):
-                    if len(us) > 1:
-                        for v in us:
+                # If we want the common-time for all nodes
+                for u, ts in self.sorted_df('ts').itertuples():
+                    if prev is None:
+                        active_set, prev = {u}, ts
+                    elif ts != prev:
+                        if len(active_set) > 1:
+                            for v in active_set:
+                                # update their common time to the ammount of all the other coexisting nodes
+                                if v in valid_nodes:
+                                    ct[v] += (len(active_set)-1)
+                        active_set, prev = {u}, ts
+                    else:
+                        active_set.add(u)
+                    if len(active_set) > 1:
+                        for v in active_set:
+                            # update their common time to the ammount of all the other coexisting nodes
                             if v in valid_nodes:
-                                ct[v] += (len(us)-1)
+                                ct[v] += (len(active_set)-1)
             return NodeCollection(ct)
         else:
             if bool(self):
                 idx = (self.df.u == u)
                 if idx.any():
                     a, b = self.df[idx], self.df[~idx]
+                    # For a single node this is equivalent to the amount
+                    # of intersection between two groups of time-sets
                     return a.intersection_size(b)
             return 0.
 
     def _common_time_pair_discrete(self, l=None):
         if l is None or self._common_time_pair__list_input(l):
             carrier = defaultdict(set)
+            # For each node take the set of time-stamps.
             for u, ts in iter(self):
                 carrier[u].add(ts)
-
+    
+            # Take all the valid pairs of nodes.
             valid_links = (combinations(set(carrier.keys()), 2) if l is None else set(l))
+            # And for all of them take the size of the intersection between all their time-sets.
             return LinkCollection({(u, v): len(carrier[u] & carrier[v]) for u, v in iter(valid_links)})
         else:
             u, v = l
