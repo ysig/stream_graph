3261e14465ac6762de8a17efde4b67337775b954
ysig <y.siglidis@gmail.com>
2019-09-13 18:38:37 +0200

    trying a fix for ci-script

--- ci_scripts/circleci/install.sh
+++ ci_scripts/circleci/install.sh
@@ -22,18 +22,16 @@ sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes in
 
 
 # Install phantom js
-sudo apt-get install chrpath libssl-dev libxft-dev -y
+sudo apt-get install openssl chrpath libssl-dev libxft-dev -y
 sudo apt-get install libfreetype6 libfreetype6-dev -y
 sudo apt-get install libfontconfig1 libfontconfig1-dev -y
-pwd
-cd ~
 export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
 wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 sudo mv $PHANTOM_JS /usr/local/share
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
 phantomjs --version
-pwd
+cd ~/project
 
 # Install project
 python setup.py clean
